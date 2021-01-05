8b83ea82d19e6c6e3d9be32d5e81b0b09d7996b8
groot <groot@localhost.localdomain>
2021-01-04 17:25:28 -0700

    day-1-band-name-generator

---
+++ day-1-band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+user_city = input("What city did you grow up in?\n")
+user_pet = input("What is the name of your pet?\n")
+print("Your band name is " + user_city + " " + user_pet)
