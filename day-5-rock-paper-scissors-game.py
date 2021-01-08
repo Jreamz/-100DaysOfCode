c1575789207ca7bb01b4fcd83bead7559fa6429f
Jreamz <38327173+Jreamz@users.noreply.github.com>
2021-01-08 01:41:49 -0700

    Update day-5-rock-paper-scissors-game.py

--- day-5-rock-paper-scissors-game.py
+++ day-5-rock-paper-scissors-game.py
@@ -26,26 +26,25 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
+rps = [rock, paper, scissors]
 user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
 
 computer = random.randint(0,2)
 
-rps = [rock, paper, scissors]
-
-print(rps[user])
-
-print("Computer chose:")
-
-print(rps[computer])
-
-if user == computer:
-  print("DRAW!")
-elif user == 0 and computer == 2:
-  print ("You Win!")
-elif user == 1 and computer == 0:
-  print("You Win!")
-elif user == 2 and computer == 1:
-  print("You Win!")
+if user >= 3 or user <= 0:
+   print("You chose an invalid number, you lose!")
 else:
-  print("You lose!")
+  print(rps[user])
+  print("Computer chose:")
+  print(rps[computer])
+
+  if user == computer:
+    print("DRAW!")
+  elif user == 0 and computer == 2:
+    print("You Win!")
+  elif user == 1 and computer == 0:
+    print("You Win!")
+  elif user == 2 and computer == 1:
+    print("You Win!")
+  else:
+    print("You lose!")
