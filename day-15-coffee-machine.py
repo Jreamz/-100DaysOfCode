f36c2e092c8029b98cf98025bad4e469e730390e
Jreamz <38327173+Jreamz@users.noreply.github.com>
2021-01-23 23:06:39 +0000

    Update day-15-coffee-machine.py

--- day-15-coffee-machine.py
+++ day-15-coffee-machine.py
@@ -31,15 +31,15 @@ resources = {
 }
 
 
-MACHINE_OFF = False
 MONEY = 0
+PROFIT = 0
 
 
 def report():
-    print(f"Water: {resources['water']} ")
-    print(f"Milk: {resources['milk']} ")
-    print(f"Coffee: {resources['coffee']} ")
-    print(f"Money: {MONEY}")
+    print(f"Water: {resources['water']}ml ")
+    print(f"Milk: {resources['milk']}ml ")
+    print(f"Coffee: {resources['coffee']}g ")
+    print(f"Profit: ${PROFIT}")
 
 
 def user_choice(choice):
@@ -54,19 +54,16 @@ def user_choice(choice):
         resources["water"] -= 250
         resources['milk'] -= 150
         resources["coffee"] -= 24
-    elif choice == "off":
-        global MACHINE_OFF
-        MACHINE_OFF = True
-    elif choice == "report":
-        return report()
 
 
 def check_resources(choice):
     global MONEY
+    global PROFIT
     if choice == "espresso":
         if resources["water"] >= 50 and resources["coffee"] >= 18 and MONEY >= 1.5:
-            MONEY %= 1.5
-            print(f"Here is your change: {MONEY}")
+            change = MONEY - PROFIT
+            PROFIT += 1.5
+            print(f"Here is your change: {change}")
             print("Here is your espresso! Enjoy!")
         elif resources["water"] < 50:
             print("Sorry not enough water.")
@@ -77,8 +74,9 @@ def check_resources(choice):
             print("Not enough money, you've been refunded.")
     elif choice == "latte":
         if resources["water"] >= 200 and resources["milk"] >= 150 and resources["coffee"] >= 24 and MONEY >= 2.5:
-            MONEY %= 2.5
-            print(f"Here is your change: {MONEY}")
+            change = MONEY - PROFIT
+            PROFIT += 2.5
+            print(f"Here is your change: {change}")
             print("Here is your latte! Enjoy!")
         elif resources["water"] < 200:
             print("Sorry not enough water.")
@@ -91,8 +89,9 @@ def check_resources(choice):
             print("Sorry not enough money, you've been refunded.")
     elif choice == "cappuccino":
         if resources["water"] >= 200 and resources["milk"] >= 150 and resources["coffee"] >= 24 and MONEY >= 3.0:
-            MONEY %= 3.0
-            print(f"Here is your change: {MONEY}")
+            change = MONEY - PROFIT
+            PROFIT += 3.0
+            print(f"Here is your change: {change}")
             print("Here is your cappuccino! Enjoy!")
         elif resources["water"] < 200:
             print("Sorry not enough water.")
@@ -109,18 +108,31 @@ def check_resources(choice):
 
 def user_money():
     global MONEY
-    quarters = float(input("How many quarters?: ")) * .25
-    dimes = float(input("How many dimes?: ")) * .10
-    nickles = float(input("How many nickels?: ")) * .05
-    pennies = float(input("How many pennies?: ")) * .01
-    MONEY = quarters + dimes + nickles + pennies
+    MONEY = int(input("How many quarters?: ")) * .25
+    MONEY += int(input("How many dimes?: ")) * .10
+    MONEY += int(input("How many nickels?: ")) * .05
+    MONEY += int(input("How many pennies?: ")) * .01
     return MONEY
 
 
+MACHINE_OFF = False
+
 while not MACHINE_OFF:
     user_input = input("What would you like? (espresso/latte/cappuccino): ")
-    user_money()
     if user_input == "off":
         MACHINE_OFF = True
-    check_resources(user_input)
-    user_choice(user_input)
+    elif user_input == "report":
+        report()
+    else:
+        user_money()
+        if MONEY >= 1.5:
+            check_resources(user_input)
+            user_choice(user_input)
+        elif MONEY >= 2.5:
+            check_resources(user_input)
+            user_choice(user_input)
+        elif MONEY >= 3.0:
+            check_resources(user_input)
+            user_choice(user_input)
+        else:
+            print("Not enough money, you've been refunded.")
